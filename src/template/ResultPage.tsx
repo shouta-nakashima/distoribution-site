90898c30edc25e69e783ab8eedb6db3c514c3d07
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-07-08 02:19:09 +0900

    Update ResultPage.tsx

--- src/template/ResultPage.tsx
+++ src/template/ResultPage.tsx
@@ -1,9 +1,23 @@
 import React, {FC} from 'react'
+import Pencil from '../assets/img/pencil.jpg'
+import {createStyles,makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    backGround: {
+      backgroundImage: `url(${Pencil})`,
+      height: '100vh',
+      backgroundSize: 'cover',
+      color: '#fff'
+    }
+  })
+)
 
 const ResultPage: FC = () => {
+  const classes = useStyles()
   return (
-    <div>
-      ResultPage 検索結果
+    <div className={classes.backGround}>
+      ResultPage 検索結果 
     </div>
   )
 }
